265a3fcf1ff60d9a7116cabc8556d3ed52bab265
Sachi Goel <67666178+computer-s-2@users.noreply.github.com>
2024-07-27 13:07:02 -0700

    finished fill-out-dps

--- fill-out-dps.py
+++ fill-out-dps.py
@@ -43,3 +43,67 @@ while x != 0 or count != 10000:
 
 
     time.sleep(3)
+
+
+    first_name = browser.find_element_by_xpath('/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[2]/div/div[1]/div/input')
+    first_name.send_keys("Sachi")
+    time.sleep(.25)
+    last_name = browser.find_element_by_xpath('/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[3]/div/div[1]/div/input')
+    last_name.send_keys("Goel")
+    time.sleep(.25)
+    dob = browser.find_element_by_xpath('/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[4]/div/div[1]/div[1]/input')
+    dob.send_keys("**") # Replaced actual birthday value
+    time.sleep(.25)
+    ssn = browser.find_element_by_xpath('/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[5]/div/div[1]/div[1]/input')
+    ssn.send_keys("**") # Replaced actual last 4 digits of ssn
+    time.sleep(3)
+
+
+    submit = browser.find_element_by_xpath('/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[4]/button')
+    submit.click()
+    print("submit")
+
+
+    time.sleep(.25)
+
+
+    new_app = browser.find_element_by_xpath('/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/div[3]/div/button')
+    new_app.click()
+    time.sleep(.25)
+    ok = browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div[2]/button')
+    ok.click()
+    time.sleep(.25)
+
+
+    renew_dl = browser.find_element_by_xpath('/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/main/div/div/div[1]/div[2]')
+    renew_dl.click()
+    time.sleep(.25)
+    email = browser.find_element_by_xpath('/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[1]/div/div[5]/div/div/div/div[1]/div/input')
+    email.send_keys("sachigoel27@gmail.com")
+    time.sleep(.25)
+    email_v = browser.find_element_by_xpath('/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[1]/div/div[6]/div/div/div/div[1]/div/input')
+    email_v.send_keys("sachigoel27@gmail.com")
+    time.sleep(.25)
+    zip = browser.find_element_by_xpath('/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[2]/div[1]/div[4]/div[1]/div/div/div[1]/div[1]/input')
+    zip.send_keys("75025")
+    time.sleep(.25)
+    next = browser.find_element_by_xpath('/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[2]/div[2]/div/div[2]/button')
+    next.click()
+    time.sleep(.25)
+    date = browser.find_element_by_xpath('/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[1]/div/table/tbody/tr/td[3]').text
+    date =  date.replace("/", "")
+    date = date[-7:]
+    print (date)
+
+
+    if int(date) < 6302022:
+        print ("woah")
+        x = 0
+        winsound.Beep(frequency, duration)
+        break
+    else:
+        count = count + 1
+        time.sleep(450)
+        browser.close()
+        print("not cool")
+        print(count)
